da46841b5b25f3ac528a03ae7b7e54766f20728d
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-04-29 11:29:47 +0200

    Add: Server ready.

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+
+try {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT}`);
+    });
+} catch (error) {
+    console.log(error);
+}
+
+app.get("/reviews", (req, res) => {
+    const apiKey = process.env.GOOGLE_API_KEY;
+    const placeName = process.env.PLACE_NAME;
+
+    try {
+        fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&fields=place_id&key=${apiKey}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const placeId = data.candidates[0].place_id;
+
+                fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        return res.status(200).json({
+                            success: true,
+                            data: data.result.reviews,
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        return res.status(500).json({
+                            success: false,
+                            message: "Internal server error",
+                        });
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+});
